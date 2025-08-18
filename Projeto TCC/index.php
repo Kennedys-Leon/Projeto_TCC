cd26d4d312926152e8afcb9c28e2a39e6261f609
Sliz <joaovitortap624@gmail.com>
2025-08-18 14:52:22 -0300

    Mudança Na Logo do Usuario (Perfil)

--- Projeto TCC/index.php
+++ Projeto TCC/index.php
@@ -286,7 +286,7 @@ session_start();
                 </div>
             <?php elseif (isset($_SESSION['vendedor_nome'])): ?>
                 <div class="usuario-box">
-                    <img src="img/user-icon.png" alt="Vendedor" class="usuario-icone-img">
+                    <img src="https://i.pinimg.com/736x/9f/4c/f0/9f4cf0f24b376077a2fcdab2e85c3584.jpg" alt="Vendedor" class="usuario-icone-img">
                     <?= htmlspecialchars($_SESSION['vendedor_nome']) ?>
                     <form action="logout.php" method="post" style="margin: 0;">
                         <button class="logout-btn" type="submit">Sair</button>
@@ -294,7 +294,7 @@ session_start();
                 </div>
             <?php else: ?>
                 <div class="usuario-box">
-                    <img src="img/user-icon.png" alt="Usuário" class="usuario-icone-img">
+                    <img src="https://i.pinimg.com/736x/9f/4c/f0/9f4cf0f24b376077a2fcdab2e85c3584.jpg" alt="Usuário" class="usuario-icone-img">
                     Usuário
                 </div>
             <?php endif; ?>
